4c3fa5c54e759ce16294c0856c33183f0be3d35d
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 23:08:22 +0900

    Title: 입력받아 계산, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240929/입력받아 계산/input-calculate.java
+++ 240929/입력받아 계산/input-calculate.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a;
-        a = sc.nextint();
+        a = sc.nextInt();
         System.out.println(a);
     }
 }
