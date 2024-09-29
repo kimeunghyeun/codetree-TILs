292e8a643b7602a260d7714ebdaa88c3ec4a3e5d
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 18:44:31 +0900

    Title: 소수점 반올림하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 240929/소수점 반올림하기/rounding-decimal-points.java
+++ 240929/소수점 반올림하기/rounding-decimal-points.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         double a = 25.352;
-        System.out.print(.1a);
+        System.out.printf("%.1f", a);
     }
 }
