553062badd803c2b577d171f15c80ad04b873d45
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-30 00:36:52 +0900

    Title: 길이 단위 환산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240930/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double ftTocm = 30.48;
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextDouble();
+        System.out.print(a * ftTocm);
+
+    }
+}
