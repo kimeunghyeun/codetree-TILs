51fd10d55ba258bf61944765dcaf5d18e4317b82
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 23:30:09 +0900

    Title: 점수 입출력, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240929/점수 입출력/enter-int-and-print-score.java
+++ 240929/점수 입출력/enter-int-and-print-score.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int a;
-        a = nextInt();
+        a = sc.nextInt();
         System.out.printf("your score is %d point", a);
         
     }
