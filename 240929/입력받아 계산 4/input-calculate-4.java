fe217b638498d2e968d4a67326d33f7e3cd13605
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 23:53:48 +0900

    Title: 입력받아 계산 4, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/입력받아 계산 4/input-calculate-4.java
+++ 240929/입력받아 계산 4/input-calculate-4.java
@@ -5,6 +5,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.printf(a * 2);
+        System.out.print(a * 2);
     }
 }
