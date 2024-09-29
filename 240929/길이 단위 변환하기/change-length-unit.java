237b1518dd382133d1f29052f4d2e7d1ce6af657
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 18:49:48 +0900

    Title: 길이 단위 변환하기, Time: 104ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240929/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double oneFit = 30.48;
+        int oneMile = 160934;
+        double a = oneFit * 9.2;
+        double b = oneMile * 1.3;
+
+        System.out.printf("%fft = %.1fcm\n", 9.2, a);
+        System.out.printf("%fmi = %.1fcm", 1.3, b);
+    }
+}
