65c975241df3cab5e3da5595d6c1691ec331610b
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-09-29 22:42:47 +0900

    Title: 변수 값 교체하기 5, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

--- 240929/변수 값 교체하기 5/replacing-variable-values-5.java
+++ 240929/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -3,6 +3,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 2, b = 6;
         a = b;
-        System.out.print("%d\n%d", a, b);
+        System.out.printf("%d\n%d", a, b);
     }
 }
