c4e81d9777b34ded34a17b1ae97b1395616688f6
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-01 23:15:50 +0900

    Title: 실수 입력받아 계산 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241001/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 241001/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -5,6 +5,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         a = sc.nextDouble();
-        System.out.printf("%.2f", a + 1.5)
+        System.out.printf("%.2f", a + 1.5);
     }
 }
