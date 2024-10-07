921b372dc06f4b931cd7d59fdf08468eda9a899c
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-07 14:37:35 +0900

    Title: 삼항연산자, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/삼항연산자/ternary-operator.java
+++ 241007/삼항연산자/ternary-operator.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        String c = a == 100 ? "pass" : "fail";
+        String c = a == 100 ? "pass" : "failure";
         System.out.print(c);
     }
 }
