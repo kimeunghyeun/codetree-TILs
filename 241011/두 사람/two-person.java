39a68cff25e5f6b4e2f08d44276b16d5c8c6bcb2
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 16:18:19 +0900

    Title: 두 사람, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 241011/두 사람/two-person.java
+++ 241011/두 사람/two-person.java
@@ -5,8 +5,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt(), char b = sc.next().charAt(0);
-        int c = sc.nextInt(), char d = sc.next().charAt(0);
+        int a = sc.nextInt();
+        char b = sc.next().charAt(0);
+        int c = sc.nextInt();
+        char d = sc.next().charAt(0);
 
         if ((a >= 19 && b == 'M') || (c >= 19 && d == 'M')) {
             System.out.print(1);
