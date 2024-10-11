f1095a29f8010b12ea7842064b3e99fce4613b9d
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 09:00:08 +0900

    Title: 출석 부르기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241011/출석 부르기/calling-attendance.java
+++ 241011/출석 부르기/calling-attendance.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.print("John");
         } else if (a == 2) {
             System.out.print("Tom");
-        } else if {
+        } else if (a == 3) {
             System.out.print("Paul");
         } else {
             System.out.print("Vacancy");
