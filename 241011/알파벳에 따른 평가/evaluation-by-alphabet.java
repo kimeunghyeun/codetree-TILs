e9525215822ab69daf0cbb13702a2846ab601348
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 09:05:07 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241011/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+
+        if (n == "S") {
+            System.out.print("Superior");
+        } else if (n == "A") {
+            System.out.print("Excellent");
+        } else if (n == "B") {
+            System.out.print("Good");
+        } else if (n == "C") {
+            System.out.print("Usually");
+        } else if (n == "D") {
+            System.out.print("Effort");
+        } else {
+            System.out.print("Failure")
+        }
+
+
+    }
+}
