d82000b38df8a152c36b6bfc00a9d274a441cb49
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 17:31:44 +0900

    Title: 코로나 메뉴얼, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241011/코로나 메뉴얼/covid-manual.java
+++ 241011/코로나 메뉴얼/covid-manual.java
@@ -13,64 +13,19 @@ public class Main {
         char Csysmtom = sc.next().charAt(0);
         int Ct = sc.nextInt();
 
-        char Aresult, Bresult, Cresult;
-
-        if (Asysmtom == 'Y') { 
-            if (At >= 37) {
-                Aresult = 'A';
-            } else if (At < 37) {
-                Aresult = 'C';
+        if (Asysmtom == 'Y' && At >= 37) {
+            if ((Bsysmtom == 'Y' && Bt >= 37) || (Csysmtom == 'Y' && Ct >= 37)){
+                System.out.print('E');
+            } else {
+                System.out.print('N');
             }
         } else {
-            if (At >= 37) {
-                Aresult = 'B';
+            if ((Bsysmtom == 'Y' && Bt >= 37) && (Csysmtom == 'Y' && Ct >= 37)){
+                System.out.print('E');
             } else {
-                Aresult = 'D';
-            }
-        }
-//--------------------------------------------------------
-        if (Bsysmtom == 'Y') { 
-            if (Bt >= 37) {
-                Bresult = 'A';
-            } else if (At < 37) {
-                Bresult = 'C';
-            }
-        } else {
-            if (Bt >= 37) {
-                Bresult = 'B';
-            } else {
-                Bresult = 'D';
-            }
-        }
-//--------------------------------------------------------
-        if (Csysmtom == 'Y') { 
-            if (Ct >= 37) {
-                Cresult = 'A';
-            } else if (At < 37) {
-                Cresult = 'C';
-            }
-        } else {
-            if (Ct >= 37) {
-                Cresult = 'B';
-            } else {
-                Cresult = 'D';
-            }
-        }
-//--------------------------------------------------------
-//--------------------------------------------------------
-        char list[] = {Aresult, Bresult, Cresult};
-        int num;
-
-        for (char e : list) {
-            if (e == 'A') {
-                num += 1;
+                System.out.print('N');
             }
         }
 
-        if (num >= 2) {
-            System.out.print('E');
-        } else {
-            System.out.print('N');
-        }
     }
 }
