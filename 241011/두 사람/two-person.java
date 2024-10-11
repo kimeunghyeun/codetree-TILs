8e3e402413528340406509c76b852d5043ea4156
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 16:11:08 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241011/두 사람/two-person.java
+++ 241011/두 사람/two-person.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a = sc.nextInt()
+        int a = sc.nextInt();
         char b = sc.next().charAt(0);
-        int c = sc.nextInt()
+        int c = sc.nextInt();
         char d = sc.next().charAt(0);
 
         if ((a >= 19 && b == 'M') || (c >= 19 && d == 'M')) {
