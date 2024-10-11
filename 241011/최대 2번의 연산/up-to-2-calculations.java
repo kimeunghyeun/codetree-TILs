5124fc705c1f32287bc525b92f3542bdf1103a45
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 11:13:20 +0900

    Title: 최대 2번의 연산, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241011/최대 2번의 연산/up-to-2-calculations.java
+++ 241011/최대 2번의 연산/up-to-2-calculations.java
@@ -7,11 +7,11 @@ public class Main {
         int a = sc.nextInt();
 
         if (a%2 == 0) {
-            a = a / 2
+            a = a / 2;
         }
 
         if (a%2 == 1) {
-            a = (a+1)/2
+            a = (a+1) / 2;
         }
     }
 }
