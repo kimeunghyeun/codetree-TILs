1e8b01b42c4225e91d165e10f07312a87284206b
kimeunghyeun <161837535+kimeunghyeun@users.noreply.github.com>
2024-10-11 16:18:47 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241011/두 사람/two-person.java
+++ 241011/두 사람/two-person.java
@@ -10,7 +10,7 @@ public class Main {
         int c = sc.nextInt();
         char d = sc.next().charAt(0);
 
-        if ((a >= 19 && b == 'M') || (c >= 19 && d == 'M')) {
+        if ((a >= 19 && b.equals('M')) || (c >= 19 && d == 'M')) {
             System.out.print(1);
         } else {
             System.out.print(0);
